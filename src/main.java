b387865921d43d740b0eb0a10829d1549d741a3a
nufer <simseknufer1987@gmail.com>
2021-05-04 02:35:18 +0300

    change

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         customerManager customerManager=new customerManager();
         customerManager.add(new databaseLogger());
+        System.out.println("git neew");
     }
 }
